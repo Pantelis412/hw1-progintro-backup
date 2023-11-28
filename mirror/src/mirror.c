3ffe79ff930993b50e851b78f0db30e0486cb396
Pantelis <sdi2300079@di.uoa.gr>
2023-11-29 00:58:58 +0200

    finished mirror.c

--- mirror/src/mirror.c
+++ mirror/src/mirror.c
@@ -1,3 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+int IsPrime(int n)
+{
+    if (!(n % 2))
+    {
+        return 0;
+    }
+    for (int i = 3; i <= sqrt(n); i += 2){
+            if (!(n % i))
+                return 0;
+        }
+    return 1;
+}
+long long int mirror(long long int n){
+    long long int result=0;
+    int tmp;
+    while(n>0){
+        tmp=(n%10);
+        result= 10*result+tmp;
+        n/=10;
+    }
+    return result;
+}
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        printf("The programm must be called as ./mirror low high\n");
+        return 1;
+    }
+    long long int low = atoll(argv[1]);
+    long long int high = atoll(argv[2]);
+    if (high < low)
+        return 1;
+    if (low < 1 || high > 1e+15)
+        return 1;
+    long long int i;
+    long long int sum=0;
+    for (i = (ceil(sqrt(low))); i <= (floor(sqrt(high))); i++)
+    {
+        if(IsPrime(i) && (sqrt(mirror(i*i))==(int)(sqrt(mirror(i*i))) && IsPrime(mirror(sqrt(i*i)))) && mirror(i*i)!=i*i){
+            sum+=i*i;
+        }
+    }
+    printf("%lld\n",sum);
+}
