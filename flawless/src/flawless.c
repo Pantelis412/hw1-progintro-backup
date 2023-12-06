a2c6ed7e07eea6fe2009bedaf52b2bd78af19d68
Pantelis <sdi2300079@di.uoa.gr>
2023-12-06 18:44:46 +0200

    bug fixes corrected reamde

--- flawless/src/flawless.c
+++ flawless/src/flawless.c
@@ -1,7 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-long long int one_digit(long long int n) {
+long long int one_digit(long long int n) {/*παρόμοια συνάρτηση με αυτή που έφτιαξα στη mirror που αθροίζει τα ψηφία ενός αριθμού*/
   long long int sum = 0;
   int tmp;
   while (n > 0) {
@@ -9,7 +9,7 @@ long long int one_digit(long long int n) {
     sum += tmp;
     n /= 10;
   }
-  return sum * sum;
+  return sum * sum;/*επιστρέφει το τετράγωνο του αθροίσματος των ψηφίων του αριθμού που έλαβε*/
 }
 int main(int argc, char **argv) {
   if (argc != 3) {
@@ -21,11 +21,11 @@ int main(int argc, char **argv) {
   if (high < low) return 1;
   if (low < 1 || high > 1e+12) return 1;
   long long int sum = 0;
-  for (long long int i = (ceil(sqrt(low))); i <= (floor(sqrt(high))); i++) {
-    if ((i * i) == one_digit(i * i)) {
+  for (long long int i = (ceil(sqrt(low))); i <= (floor(sqrt(high))); i++) {/*όμοια τακτική που ακολουθήσαμε στη mirror για να πάρουμε μόνο τα τέλεια τετράγωνα*/
+    if ((i * i) == one_digit(i * i)) {/*αν ο αριθμός-που είναι τέλιο τετράγωνο- ισούται με το τετράγωνο του αθροίσματος των ψηφίων του τον συμπεριλαμβάνουμε στο άθροισμά μας*/
       sum += (i * i);
     }
   }
-  printf("%lld\n", sum);
+  printf("%lld\n", sum);/*τυπώνουμε το άθροισμα που βρήκαμε*/
   return 0;
 }
